e7eab98419c551e050d41b5e238a46e4294ab6a8
Jee <jeecoding09@gmail.com>
2026-08-22 11:38:45 +0800

    feat: Added a new column "total_stock" for categories implementation of total revenue

--- src/db/query.ts
+++ src/db/query.ts
@@ -2,10 +2,12 @@ import type { Categories, Item } from '../types.ts'
 import type { QueryResult } from 'pg'
 import pool from './pool.ts'
 
+// Add new column for items "total stock" that derives from stock
+
 async function fetchAllItems(): Promise<QueryResult<Item>> {
    return await pool.query(
       `
-         SELECT items.name as item, price, stock, cost, unit, status, categories.name as category FROM items
+         SELECT items.name as item, price, stock, total_stock, cost, unit, status, categories.name as category FROM items
          LEFT JOIN categories
          ON items.category_id = categories.category_id
       `,
